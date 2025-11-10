5ad71a6bdfee4579be1a2e54de4742abd9f1efb7
Halfmoon <kopco133596@gmail.com>
2025-11-10 16:13:43 +0800

    update

--- TFT simulator/obj/Release/net8.0-windows/TFT simulator.AssemblyInfo.cs
+++ TFT simulator/obj/Release/net8.0-windows/TFT simulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFT simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e75d1e967dc1533ee3990530b11c2e8e39f9d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFT simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFT simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
